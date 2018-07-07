4eb2cdb2c97b309715f8609784f1e7fa557bc0b3
Andy Truong <att35663@uga.edu>
2018-07-07 16:01:50 -0400

    Formatted code and added comments

--- script.js
+++ script.js
@@ -8,42 +8,66 @@
 
     var app = angular.module('myApp', []);
     app.filter('addComma', function() {
-       return function(x) {
+        
+       // A filter that adds comma to the correct location for large numbers
+       return function(x) 
+       {
            var tempString = [];
             var i;
             var j = 0; 
             var marketCap = String(x);
-            for (i = marketCap.length; i > 0; i--) { 
-            if(i % 3 == 0 && i != marketCap.length) {
+            for (i = marketCap.length; i > 0; i--) 
+            { 
+                if(i % 3 == 0 && i != marketCap.length) 
+            {
                 tempString += ',' + marketCap[j];
                 j++;
             }
-            else { 
+            else 
+            { 
                 tempString += marketCap[j];
                 j++;
- 
             }
         }
         return tempString;
        } 
+       
+       
     });
     app.controller('myCtrl', function($scope, $http) {
-        $scope.myObj = {
-            color : "red"
-        }
+        
+        /*
+        * Request data for top 10 cryptocurrencies by total 
+        * marketcap from CoinMarketCap API
+        */
         $http.get("https://api.coinmarketcap.com/v2/ticker/?limit=10&sort=rank&structure=array")
             .then(function(myArr) {
             $scope.myWelcome = myArr;
         });
         
+        /*
+        * Request data for global information about cryptocurrencies
+        * from CoinMarketCap API
+        */
         $http.get("https://api.coinmarketcap.com/v2/global/")
             .then(function(myArr) {
             $scope.global = myArr.data.data;
         });
+        
+        
+        
     });
    
        
     
+
+
+
+
+
+
+
+
 
  function addComma(strMarketCap) {
         var tempString = [];
