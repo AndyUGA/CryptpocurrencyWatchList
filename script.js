0731862f7a0ce7713d2c5a50d09354aa2cc4d9d5
Andy Truong <att35663@uga.edu>
2018-07-07 10:48:09 -0400

    Used ng-repeat to display top 10 coins by marketcap

--- script.js
+++ script.js
@@ -2,13 +2,8 @@
 
 
 
-
-    //var num = 0;
-     var testArray = {};
-     var carName = "car";
-     var x = 0;
-    console.log(testArray);
-
+    var data = [];
+    
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() 
@@ -16,13 +11,7 @@
         if (this.readyState == 4 && this.status == 200) 
         {
         var myArr = JSON.parse(this.responseText);
-
-            
-            
-            
-            
-            
-
+        
         document.getElementById("coinPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             
         document.getElementById("coinName").innerHTML = myArr.data.name;
@@ -49,8 +38,6 @@
             document.getElementById("1Change").style.color="red";
         }
        
-        
-            
         var marketCap = myArr.data.quotes.USD.market_cap;
         var stringMarketCap = addComma(String(marketCap));
         document.getElementById("coinMarketCap").innerHTML = stringMarketCap;
@@ -61,100 +48,74 @@
         xmlhttp.send();
 
 
-   /*
+
     var xmlhttp2 = new XMLHttpRequest();
     xmlhttp2.onreadystatechange = function() 
     {
+    
         if (this.readyState == 4 && this.status == 200) 
         {
+               
             var myArr = JSON.parse(this.responseText);
-            
-            
-     
-        document.getElementById("coinPrice2").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
-            
-        document.getElementById("coinName2").innerHTML = myArr.data.name;
+               
+            for(var i = 0; i < 10; i++) 
+                {
+                    data[i] = {
+                        name : myArr.data[i].name,
+                        price : myArr.data[i].quotes.USD.price,
+                        tfChange : myArr.data[i].quotes.USD.percent_change_24h,
+                        marketCap : myArr.data[i].quotes.USD.market_cap,
+                        rank: myArr.data[i].rank,
+                        oneHourChange: myArr.data[i].quotes.USD.percent_change_1h,
+                    }
+                }
+                
+                
+
+        }
         
-        var twentyFourHourChange = myArr.data.quotes.USD.percent_change_24h; 
-        if(twentyFourHourChange > 0) {
-            document.getElementById("coinChange2").innerHTML = String(twentyFourHourChange) + '%';
-            document.getElementById("coinChange2").style.color="#009e73";
-        }
-        else {
-            document.getElementById("coinChange2").innerHTML = String(twentyFourHourChange) + '%';
-            document.getElementById("coinChange2").style.color="red";
-        }
-            
-        var marketCap = myArr.data.quotes.USD.market_cap;
-        var stringMarketCap = addComma(String(marketCap));
-        document.getElementById("coinMarketCap2").innerHTML = stringMarketCap;
-  
-        }
-      
+        
     };
-    xmlhttp2.open("GET", "https://api.coinmarketcap.com/v2/ticker/1376/", true);
+
+
+        
+      
+    xmlhttp2.open("GET", "https://api.coinmarketcap.com/v2/ticker/?limit=10&sort=rank&structure=array", true);
     xmlhttp2.send();
 
-      */
 
 
-    var app = angular.module("myApp", []);
-    app.controller("myCtrl", function($scope) {
-
-    var xmlhttp3 = new XMLHttpRequest();
-    xmlhttp3.onreadystatechange = function() 
-    {
-        
-        if (this.readyState == 4 && this.status == 200) 
+    var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+  $http.get("https://api.coinmarketcap.com/v2/ticker/?limit=10&sort=rank&structure=array")
+  .then(function(myArr) {
+     $scope.myWelcome = myArr;
+      var jsonData =  $scope.myWelcome;     
+      /*
+      for(var i = 0; i < 10; i++)
         {
+            data[i] = {
+                name : jsonData.data[i].name,
+                price : jsonData.data[i].quotes.USD.price,
+                tfChange : jsonData.data[i].quotes.USD.percent_change_24h,
+                marketCap : jsonData.data[i].quotes.USD.market_cap,
+                rank: jsonData.data[i].rank,
+                oneHourChange: jsonData.data[i].quotes.USD.percent_change_1h,
 
-            var myArr = JSON.parse(this.responseText);
-            //console.log(myArr.data);
-            //console.log(myArr.data);
-            
-            /*
-            for(var key in myArr) {
-                // console.log(myArr[key][0].symbol);
-                // console.log(myArr[key][0].quotes.USD.price);
-            
             }
-            */
-            
-            var keys = [];
-             
-            for(var key in myArr)
-            {
-                if(myArr.hasOwnProperty(key)) 
-                    {
-                        keys.push(key);
-                    }
-            }
-        
-            for(var i = 0; i < keys.length; i++) 
-                {
-                    console.log(myArr[keys[0]]);
-                }
-            
-        }
-        
-        console.log(' 122 ' + window.carName);
-    };
-    xmlhttp3.open("GET", "https://api.coinmarketcap.com/v2/ticker/?limit=10&sort=rank&structure=array", true);
-    xmlhttp3.send();
-        console.log('126');
-        $scope.records = testArray
-        
+        }   
+        */
+      
+  });
+});
+   
        
-        
-         
-    }); 
+    
 
-       
 
   
     
     
-
 
 
 
@@ -167,7 +128,6 @@
         var tempString = [];
         var i;
         var j = 0; 
-        console.log("strMarketCap is " + strMarketCap.length);
         for (i = strMarketCap.length; i > 0; i--) { 
             
             if(i % 3 == 0 && i != strMarketCap.length) {
