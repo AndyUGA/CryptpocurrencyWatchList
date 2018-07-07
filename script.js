ed7b15b8166cdba6e38e772187cdcbaf3c9b02a7
Andy Truong <att35663@uga.edu>
2018-07-06 22:08:43 -0400

    Added rank and one hour change

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-/*eslint-env browser*/
-     
-    //var num = 0;
-    //var testArray = [];
 
+
+
+
+
+    //var num = 0;
+     var testArray = {};
+     var carName = "car";
+     var x = 0;
+console.log(testArray);
 
 
     var xmlhttp = new XMLHttpRequest();
@@ -11,20 +16,38 @@
         if (this.readyState == 4 && this.status == 200) 
         {
         var myArr = JSON.parse(this.responseText);
+
+            
+            
+            
+            
+            
+
         document.getElementById("coinPrice").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             
         document.getElementById("coinName").innerHTML = myArr.data.name;
         
-        var percentValue = myArr.data.quotes.USD.percent_change_24h; 
-        if(percentValue > 0) {
-            document.getElementById("coinChange").innerHTML = String(percentValue) + '%';
-            document.getElementById("coinChange").style.color="green";
+        document.getElementById("coinRank").innerHTML = myArr.data.rank;
+        
+        var twentyFourHourChange = myArr.data.quotes.USD.percent_change_24h; 
+        if(twentyFourHourChange > 0) {
+            document.getElementById("24Change").innerHTML = String(twentyFourHourChange) + '%';
+            document.getElementById("24Change").style.color="#009e73";
         }
         else {
-            document.getElementById("coinChange").innerHTML = String(percentValue) + '%';
-            document.getElementById("coinChange").style.color="red";
+            document.getElementById("24Change").innerHTML = String(twentyFourHourChange) + '%';
+            document.getElementById("24Change").style.color="red";
         }
-        //var percentChange = String(myArr.data.quotes.USD.percent_change_24h) + '%';
+            
+        var oneHourChange = myArr.data.quotes.USD.percent_change_1h; 
+        if(oneHourChange > 0) {
+            document.getElementById("1Change").innerHTML = String(oneHourChange) + '%';
+            document.getElementById("1Change").style.color="#009e73";
+        }
+        else {
+            document.getElementById("1Change").innerHTML = String(oneHourChange) + '%';
+            document.getElementById("1Change").style.color="red";
+        }
        
         
             
@@ -38,54 +61,115 @@
         xmlhttp.send();
 
 
-
+   /*
     var xmlhttp2 = new XMLHttpRequest();
     xmlhttp2.onreadystatechange = function() 
     {
         if (this.readyState == 4 && this.status == 200) 
         {
             var myArr = JSON.parse(this.responseText);
+            
+            
+     
         document.getElementById("coinPrice2").innerHTML = Number(myArr.data.quotes.USD.price).toFixed(2);
             
         document.getElementById("coinName2").innerHTML = myArr.data.name;
         
-        var percentValue = myArr.data.quotes.USD.percent_change_24h; 
-        if(percentValue > 0) {
-            document.getElementById("coinChange2").innerHTML = String(percentValue) + '%';
-            document.getElementById("coinChange2").style.color="green";
+        var twentyFourHourChange = myArr.data.quotes.USD.percent_change_24h; 
+        if(twentyFourHourChange > 0) {
+            document.getElementById("coinChange2").innerHTML = String(twentyFourHourChange) + '%';
+            document.getElementById("coinChange2").style.color="#009e73";
         }
         else {
-            document.getElementById("coinChange2").innerHTML = String(percentValue) + '%';
+            document.getElementById("coinChange2").innerHTML = String(twentyFourHourChange) + '%';
             document.getElementById("coinChange2").style.color="red";
         }
             
         var marketCap = myArr.data.quotes.USD.market_cap;
         var stringMarketCap = addComma(String(marketCap));
         document.getElementById("coinMarketCap2").innerHTML = stringMarketCap;
+  
         }
+      
     };
     xmlhttp2.open("GET", "https://api.coinmarketcap.com/v2/ticker/1376/", true);
     xmlhttp2.send();
 
+      */
 
+
+    var app = angular.module("myApp", []);
+    app.controller("myCtrl", function($scope) {
 
     var xmlhttp3 = new XMLHttpRequest();
     xmlhttp3.onreadystatechange = function() 
     {
         
+        if (this.readyState == 4 && this.status == 200) 
+        {
+
+            var myArr = JSON.parse(this.responseText);
+            //console.log(myArr.data);
+            //console.log(myArr.data);
+            
+            /*
+            for(var key in myArr) {
+                // console.log(myArr[key][0].symbol);
+                // console.log(myArr[key][0].quotes.USD.price);
+            
+            }
+            */
+            
+            var keys = [];
+             
+            for(var key in myArr)
+            {
+                if(myArr.hasOwnProperty(key)) 
+                    {
+                        keys.push(key);
+                    }
+            }
+        
+            for(var i = 0; i < keys.length; i++) 
+                {
+                    console.log(myArr[keys[0][0]]);
+                }
+            
+        }
+        
+        console.log(' 122 ' + window.carName);
     };
     xmlhttp3.open("GET", "https://api.coinmarketcap.com/v2/ticker/?limit=10&sort=rank&structure=array", true);
     xmlhttp3.send();
+        console.log('126');
+        $scope.records = testArray
         
+       
         
+         
+    }); 
 
-    function addComma(strMarketCap) {
+       
+
+  
+    
+    
+
+
+
+
+
+
+
+
+
+ function addComma(strMarketCap) {
         var tempString = [];
         var i;
         var j = 0; 
         console.log("strMarketCap is " + strMarketCap.length);
         for (i = strMarketCap.length; i > 0; i--) { 
-            console.log('i is ' + i);
+            
             if(i % 3 == 0 && i != strMarketCap.length) {
                 tempString += ',' + strMarketCap[j];
                 j++;
