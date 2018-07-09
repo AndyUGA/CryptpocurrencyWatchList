88a2cfaa962881a944cf434abe5a7b4e41f58735
Andy Truong <att35663@uga.edu>
2018-07-08 23:14:27 -0400

    Removed rank by market cap and changed to volume traded within last 24 hours

--- script.js
+++ script.js
@@ -39,18 +39,25 @@
     
 
 
-    app.controller('myCtrl', function($scope, $http) {
+    app.controller('myCtrl', function($scope, $http, $timeout) {
         
         
         
         /*
-        * Request data for top 10 cryptocurrencies by total 
+        * Request data for top 20 cryptocurrencies by total 
         * marketcap from CoinMarketCap API
         */
         $http.get("https://api.coinmarketcap.com/v2/ticker/?limit=20&sort=rank&structure=array")
             .then(function(myArr) {
             $scope.myWelcome = myArr;
         });
+        
+        
+        
+        
+        
+        
+        
         
         /*
         * Request data for global information about cryptocurrene 
